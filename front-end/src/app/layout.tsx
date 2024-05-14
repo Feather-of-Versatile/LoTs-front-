5e6dd2cc149a8b3aace5f8a2af8874a4dd0c435a
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-05-14 17:50:14 +0900

    feat: Noto_Sans_KR 폰트로 변경

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.scss";
+import { Noto_Sans_KR } from "next/font/google"
 import Nav from "./_compoents/nav/nav";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "LoTs",
   description: "라츠를 통한 편리한 팀구성",
 };
-
+const noto_sans = Noto_Sans_KR({ subsets: ['latin'] });
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={noto_sans.className}>
         <Nav />
         {children}
       </body>
