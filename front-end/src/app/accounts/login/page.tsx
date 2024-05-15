49182b3953a85376686201e6054f5cfdcc51ec2f
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-05-15 21:00:54 +0900

    feat: 로그인 페이지 제작
    기능 구현은 하지 않음

---
+++ front-end/src/app/accounts/login/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import S from './_style/login.module.scss'
+import Link from 'next/link'
+const page = () => {
+    return (
+        <div className={S.login_container}>
+            <h1>로그인</h1>
+            <div className={S.login_input_div}>
+                <input type='email' placeholder='이메일 주소' id='text' />
+                <input type='password' placeholder='비밀번호' id='text' />
+                <div className={S.login_check_div}>
+                    <input type='checkbox' id='check' />
+                    <p>로그인 상태 유지</p>
+                </div>
+            </div>
+            <button type='button' className={S.login_button}>
+                <p>로그인</p>
+            </button>
+            <Link href='/accounts/signup'>
+                <p>계정 생성</p>
+            </Link>
+            <p>비밀번호 찾기</p>
+        </div>
+    )
+}
+
+export default page
