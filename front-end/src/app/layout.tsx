bd43f9c6721bcea802ebf5adfbbcbb28ade43575
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-05-28 02:45:45 +0900

    feat: RootLayout의 types 추가

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,17 +1,24 @@
 import type { Metadata } from "next";
 import "./globals.scss";
-import { Noto_Sans_KR } from "next/font/google"
+import { Noto_Sans_KR } from "next/font/google";
 import Nav from "./_compoents/nav/nav";
+
 export const metadata: Metadata = {
   title: "LoTs",
   description: "라츠를 통한 편리한 팀구성",
 };
+
 const noto_sans = Noto_Sans_KR({ subsets: ['latin'] });
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+  types?: string; // 선택적 필드 추가
+}
+
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+  types, // 선택적 필드 추가
+}: Readonly<RootLayoutProps>) {
   return (
     <html lang="kr">
       <body className={noto_sans.className}>
