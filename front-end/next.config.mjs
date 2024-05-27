df4b45de67029b6c7b96520e72704a268e4283b5
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-05-28 02:14:43 +0900

    feat: svgr을 위한 webpack 설정

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -2,7 +2,16 @@
 const nextConfig = {
     sassOptions: {
         includePaths: ['styles'],
-    }
+    },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            use: ["@svgr/webpack"],
+        });
+
+        return config;
+    },
+
 };
 
 export default nextConfig;
