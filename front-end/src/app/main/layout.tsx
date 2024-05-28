e7c3c88879a5279cedcbe70dac97221d22adda33
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-05-28 19:37:28 +0900

    fix: type 제거

--- front-end/src/app/main/layout.tsx
+++ front-end/src/app/main/layout.tsx
@@ -5,12 +5,10 @@ import Nav from "../_compoents/nav/nav";
 
 interface MainLayoutProps {
     children: React.ReactNode;
-    types: string;
 }
 
 export default function MainLayout({
     children,
-    types,
 }: Readonly<MainLayoutProps>) {
     return (
         <section>
