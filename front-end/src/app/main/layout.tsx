d18ca827f6fc8ef46e21e4245820077479198155
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-05-28 19:33:54 +0900

    fix: layout type 오류 수정
    Root 로 설정이 되어 있어 오류가 발생

--- front-end/src/app/main/layout.tsx
+++ front-end/src/app/main/layout.tsx
@@ -2,23 +2,20 @@ import type { Metadata } from "next";
 import "@/app/globals.scss"
 import { Noto_Sans_KR } from "next/font/google";
 import Nav from "../_compoents/nav/nav";
-const noto_sans = Noto_Sans_KR({ subsets: ['latin'] });
 
-interface RootLayoutProps {
+interface MainLayoutProps {
     children: React.ReactNode;
     types: string;
 }
 
-export default function RootLayout({
+export default function MainLayout({
     children,
     types,
-}: Readonly<RootLayoutProps>) {
+}: Readonly<MainLayoutProps>) {
     return (
-        <html lang="kr">
-            <body className={noto_sans.className}>
-                <Nav />
-                {children}
-            </body>
-        </html>
-    );
+        <section>
+            <Nav />
+            {children}
+        </section>
+    )
 }
